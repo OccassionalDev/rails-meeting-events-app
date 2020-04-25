980675f14573995103f851aecf9318ac66f14ef3
OccassionalDev <ericaschimmel01@gmail.com>
2020-04-25 14:11:55 -0400

    created a partial for the event information and added it to the home page and the user show page to list all the events a user has, and created a dashboard page view for the current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,32 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end 
 
-    def home
-      @events = Event.all
-    end 
+  def home
+    @events = Event.all
+  end 
 
-    # Sign-Up
-    def new
-        @user = User.new
-    end 
+  # Sign-Up
+  def new
+      @user = User.new
+  end 
 
-    def create
-        @user = User.create(user_params)
+  def create
+      @user = User.create(user_params)
 
-        if @user.save
-          session[:user_id] = @user.id
-          redirect_to root_url, notice: "Account succesfully created!"
-        
-        else 
-         render "new"
-        end 
-    end 
-
-    def show 
-    end 
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to root_url, notice: "Account succesfully created!"
+      
+      else 
+        render "new"
+      end 
+  end 
 
   private 
-  
+
   def user_params(*args) 
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end 
