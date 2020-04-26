659520226f98bdbe155e314729facf615e4c746d
OccassionalDev <ericaschimmel01@gmail.com>
2020-04-26 15:01:15 -0400

    fixed syntax errors in the dashboard view and the events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,7 @@ class EventsController < ApplicationController
     def show
         @event = Event.find(params[:id])
 
-        if can_reserve(@event)
+        if can_reserve?(@event)
             @reservation = Reservation.find_by(event_id: params[:id], user_id: current_user.id)
         end 
     end 
