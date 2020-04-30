afb8366a650853ccb9b19e9822de5d7235d83615
OccassionalDev <ericaschimmel01@gmail.com>
2020-04-29 22:03:32 -0400

    redirect users back to the event show page if they've already submitted a review for that event

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,7 +6,11 @@ class ReviewsController < ApplicationController
     end 
     
     def new
-        @review = Review.new(event_id: params[:event_id])
+        if does_review_exist?(params[:event_id])
+            redirect_to event_path(Event.find(params[:event_id])), alert: "You've already submitted a review for this event."
+        else 
+            @review = Review.new(event_id: params[:event_id])
+        end 
     end 
 
     def create
@@ -24,5 +28,10 @@ class ReviewsController < ApplicationController
 
     def review_params(*args)
         params.require(:review).permit(:rating, :content, :event_id)
+    end 
+
+    def does_review_exist?(event_id)
+        review = current_user.reviews.find_by(event_id: event_id)
+        !review.nil?
     end 
 end 
